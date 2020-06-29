6d13e4cc36b9a0562744a838344433c501cbd90a
Akshdeep1998 <61948951+Akshdeep1998@users.noreply.github.com>
2020-06-29 12:11:14 +1200

    update

--- HorseRace/Megan.cs
+++ HorseRace/Megan.cs
@@ -7,8 +7,7 @@ namespace HorseRace
         public Megan(Bet bet, Label MaximumBet, int Cash, Label label) : base(bet, MaximumBet, Cash, label)
         {
         }
-
-        public override void setPunterName()
+       public override void setPunterName()
         {
             Name = "Megan";
         }
